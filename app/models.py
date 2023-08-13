ee955e896a62e7a7cc1b3ab9e3abf71033cff1b2
FarhanUn123 <ufarhan600@gmail.com>
2023-08-13 12:50:34 +0600

    update

--- app/models.py
+++ app/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Lat_news(models.Model):
-    image1=models.ImageField(upload_to='pics')
-    image2=models.ImageField(upload_to='pics')
+    image1=models.URLField(max_length=1000)
+    image2=models.URLField(max_length=1000)
     caption=models.CharField(max_length=50)
     text1=models.CharField(max_length=50)
     text2=models.CharField(max_length=50)
@@ -11,8 +11,8 @@ class Lat_news(models.Model):
 class Nex_match(models.Model):
     team1=models.CharField(max_length=50)
     team2=models.CharField(max_length=50)
-    t1_img=models.ImageField(upload_to='pics')
-    t2_img=models.ImageField(upload_to='pics')
+    t1_img=models.URLField(max_length=1000)
+    t2_img=models.URLField(max_length=1000)
     l_name=models.CharField(max_length=50)
     date=models.DateField()
     time=models.TimeField()
@@ -27,12 +27,12 @@ class Ranking(models.Model):
     pts=models.IntegerField()
 
 class Video(models.Model):
-    image=models.ImageField(upload_to='pics')
+    image=models.URLField(max_length=1000)
     caption=models.CharField(max_length=50)
     link=models.URLField(max_length=200)
 
 class Blog(models.Model):
-    image=models.ImageField(upload_to='pics')
+    image=models.URLField(max_length=1000)
     date=models.DateField()
     caption=models.CharField(max_length=50)
     text=models.TextField()
@@ -43,11 +43,10 @@ class Score(models.Model):
     a_t=models.CharField(max_length=50)
     h_s=models.IntegerField()
     a_s=models.IntegerField()
-    h_im=models.ImageField(upload_to='pics')
-    a_im=models.ImageField(upload_to='pics')
+    h_im=models.URLField(max_length=1000)
+    a_im=models.URLField(max_length=1000)
 
 class Event(models.Model):
-    # image=models.ImageField(upload_to='pics')
     image=models.URLField(max_length=1000)
     head=models.CharField(max_length=50)
     text=models.TextField()
@@ -55,12 +54,12 @@ class Event(models.Model):
 
 class Comment(models.Model):
     name=models.CharField(max_length=50)
-    image=models.ImageField(upload_to='pics')
+    image=models.URLField(max_length=1000)
     time=models.CharField(max_length=50)
     message=models.TextField()
 
 class Video2(models.Model):
-    image=models.ImageField(upload_to='pics')
+    image=models.URLField(max_length=1000)
     name=models.CharField(max_length=50)
     num=models.IntegerField()
     pos=models.CharField(max_length=50)
@@ -78,7 +77,7 @@ class Blog2(models.Model):
     date=models.DateField()
     text=models.TextField()
     p1=models.TextField()
-    image2=models.ImageField(upload_to='pics')
+    image2=models.URLField(max_length=1000)
     p2=models.TextField()
-    image3=models.ImageField(upload_to='pics')
+    image3=models.URLField(max_length=1000)
     p3=models.TextField()
